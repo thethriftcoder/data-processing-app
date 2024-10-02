4f1533a9e4b3a0513299c266c4b9c1c13ee6c3e0
thethriftcoder <the.thriftgamer@gmail.com>
2024-10-02 17:13:33 +0530

    chore: read db env vars inside application

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -1,2 +1,15 @@
+import os
+
+import dotenv
+
+
+dotenv.load_dotenv()
+
+
 # 10 MB
 MAX_SENSOR_FILE_SIZE = 1024 * 1024 * 10
+
+DB_NAME = os.environ["DB_NAME"]
+DB_HOST = os.environ["DB_HOST"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
