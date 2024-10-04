02692420cb6fcf7a69d8409004152e4069641010
thethriftcoder <the.thriftgamer@gmail.com>
2024-10-04 23:49:33 +0530

    feat: add cors middleware

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -13,3 +13,5 @@ DB_NAME = os.environ["DB_NAME"]
 DB_HOST = os.environ["DB_HOST"]
 DB_USER = os.environ["DB_USER"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173", "http://0.0.0.0:5173"]
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 from app.routers import sensor
+from app.config.config import CORS_ALLOWED_ORIGINS
 
 
 app = FastAPI(default_response_class=ORJSONResponse)
+
 app.include_router(sensor.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "HEAD"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
