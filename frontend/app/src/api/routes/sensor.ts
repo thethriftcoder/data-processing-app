15588d46597c5786a1e8d411da1bcdd8e7af3bd9
thethriftcoder <the.thriftgamer@gmail.com>
2024-10-05 13:17:07 +0530

    feat: add sensor data API in frontend
    - integrate as useMutation hook

--- frontend/app/src/api/routes/sensor.ts
+++ frontend/app/src/api/routes/sensor.ts
@@ -1,14 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { ApiResponse, AUTH_HEADER, BACKEND_URL } from "../../config";
-import { sensorFileMetadataResponse } from "../../schemas/sensor";
+import { sensorDataResponse, sensorFileMetadataResponse } from "../../schemas/sensor";
 
 const API_PREFIX = "sensor";
 const BASE_URL = `${BACKEND_URL}/${API_PREFIX}`;
+
+type getSensorDataProps = {
+  fileMetadataId: number;
+};
 
 export const useGetSensorFilesMetadata = () =>
   useQuery({
     queryKey: ["sensorFilesMetadata"],
     queryFn: getSensorFilesMetadata,
+  });
+
+export const useGetSensorData = () =>
+  useMutation({
+    mutationKey: ["sensorData"],
+    mutationFn: ({ fileMetadataId }: getSensorDataProps) => getSensorData(fileMetadataId),
   });
 
 async function getSensorFilesMetadata(): Promise<ApiResponse<sensorFileMetadataResponse>> {
@@ -22,6 +32,22 @@ async function getSensorFilesMetadata(): Promise<ApiResponse<sensorFileMetadataR
   if (!response.ok) {
     console.error("error getting sensor file metadata:", jsonData);
     throw new Error("Failed to get sensor file metadata");
+  }
+
+  return jsonData;
+}
+
+async function getSensorData(fileMetadataId: number): Promise<ApiResponse<sensorDataResponse>> {
+  const url = new URL(BASE_URL + "/data/" + fileMetadataId);
+
+  const response = await fetch(url, {
+    headers: { Authorization: AUTH_HEADER },
+  });
+  const jsonData = await response.json();
+
+  if (!response.ok) {
+    console.error("error getting sensor data:", jsonData);
+    throw new Error(`Failed to get sensor data for file metadata ID: ${fileMetadataId}`);
   }
 
   return jsonData;
--- frontend/app/src/schemas/sensor.ts
+++ frontend/app/src/schemas/sensor.ts
@@ -4,9 +4,43 @@ export interface sensorFileMetadata {
   content_type: string;
   size: number;
   upload_start_date: Date;
-  upload_end_date?: Date;
+  upload_end_date?: Date; // TODO: make this Date | null instead
+}
+
+interface hourlyDataValue {
+  time: Date;
+  value: number | null;
+}
+
+export interface sensorData {
+  id: number;
+  file_metadata_id: number;
+  elevation: number;
+  generationtime_ms: number;
+  longitude: number;
+  latitude: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  hourly_wind_speeds_100m: hourlyDataValue[];
+  hourly_wind_directions_100m: hourlyDataValue[];
+  hourly_temperatures: hourlyDataValue[];
+  hourly_humidities: hourlyDataValue[];
+  hourly_dew_points: hourlyDataValue[];
+  hourly_apparent_temperatures: hourlyDataValue[];
+  hourly_precipitations: hourlyDataValue[];
+  hourly_rains: hourlyDataValue[];
+  hourly_snowfalls: hourlyDataValue[];
+  hourly_snow_depths: hourlyDataValue[];
+  hourly_pressures_msl: hourlyDataValue[];
+  hourly_surface_pressures: hourlyDataValue[];
+  hourly_cloud_covers: hourlyDataValue[];
 }
 
 export interface sensorFileMetadataResponse {
   file_metadata_records: Array<sensorFileMetadata>;
+}
+
+export interface sensorDataResponse {
+  sensor_data: sensorData;
 }
